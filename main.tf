8341057c34888eba65444b31093867d8daf8df93
kannasac <159641966+kannasac@users.noreply.github.com>
2024-03-09 14:46:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "dev" {
     ami = "ami-0440d3b780d96b29d"
-    instance_type = "t2.medium"
+    instance_type = "t2.micro"
     tags = {
       Name = "dev-ec2"
     }
